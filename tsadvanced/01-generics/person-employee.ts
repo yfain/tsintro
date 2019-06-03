21d76cd2b312a6974230a7f4c31c94da73bfedf1
Yakov Fain <yakovfain@gmail.com>
2019-06-03 16:04:21 -0400

    added the first code sample to TS Advanced

---
+++ tsadvanced/01-generics/person-employee.ts
@@ -0,0 +1,17 @@
+class Person {  
+    name: string;
+}
+
+class Employee extends Person {  
+    department: number;
+}
+
+class Animal {  
+    breed: string;
+}
+
+const workers: Array<Person> = []; 
+
+workers[0] = new Person();  
+workers[1] = new Employee(); 
+workers[2] = new Animal(); // compile-time error 
